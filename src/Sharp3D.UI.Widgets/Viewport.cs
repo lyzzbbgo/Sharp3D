541264d35d2b897e1af770d6e435cec0c4e0c094
Piotr Wlodek <piotr.wlodek@gmail.com>
2010-08-26 21:23:00 +0200

    Initial project structure

---
+++ src/Sharp3D.UI.Widgets/Viewport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sharp3D.UI.Widgets
+{
+    public class Viewport : Control
+    {
+        static Viewport()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (Viewport), new FrameworkPropertyMetadata(typeof (Viewport)));
+        }
+    }
+}
---
+++ src/Sharp3D/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sharp3D
+{
+    /// <summary>
+    /// Interaction logic for ShellWindow.xaml
+    /// </summary>
+    public partial class ShellWindow
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
