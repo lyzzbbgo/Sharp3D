639570175048e50aaeb35b88d6331603c3ebf2c9
Piotr Wlodek <piotr.wlodek@gmail.com>
2010-08-27 19:04:18 +0200

    Initial scene classes, initial CompositeViewport control

--- src/Sharp3D.UI.Widgets/Viewport.cs
+++ src/Sharp3D.UI.Widgets/Viewport.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using Sharp3D.Scene;
 
 namespace Sharp3D.UI.Widgets
 {
@@ -9,6 +10,24 @@ namespace Sharp3D.UI.Widgets
         static Viewport()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof (Viewport), new FrameworkPropertyMetadata(typeof (Viewport)));
+        }
+
+        public static readonly DependencyProperty SceneProperty =
+            DependencyProperty.Register("Scene", typeof(IScene), typeof(Viewport),
+                new UIPropertyMetadata(null, OnSceneChanged));
+
+        /// <summary>
+        /// Gets or sets the scene. This is a dependency property.
+        /// </summary>
+        public IScene Scene
+        {
+            get { return (IScene)GetValue(SceneProperty); }
+            set { SetValue(SceneProperty, value); }
+        }
+
+        private static void OnSceneChanged(DependencyObject dp, DependencyPropertyChangedEventArgs args)
+        {
+            
         }
     }
 }
